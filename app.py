76e139ae9bf8166d607e9c291dfb700d003e4ccf
Adolfo García Vázquez <A01741767@tec.mx>
2025-11-04 15:38:59 -0600

    si

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app= Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return jsonify({"Message": "it works! Student XXXX"})
+
+if __name__ == "__main__":
+    app.run(threaded=True, host='0.0.0.0', port=3000)
